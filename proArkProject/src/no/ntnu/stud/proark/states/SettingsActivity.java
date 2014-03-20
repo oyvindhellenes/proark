dbec6a654b7bc0d9ef6d3172f4b5b42ed9f26a91
Kyrre <kyrre.l.moe@gmail.com>
2014-03-20 14:46:19 +0100

    derp

---
+++ proArkProject/src/no/ntnu/stud/proark/states/SettingsActivity.java
@@ -0,0 +1,25 @@
+package no.ntnu.stud.proark.states;
+
+import no.ntnu.stud.proark.R;
+import no.ntnu.stud.proark.R.layout;
+import no.ntnu.stud.proark.R.menu;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SettingsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_settings);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.settings, menu);
+		return true;
+	}
+
+}
