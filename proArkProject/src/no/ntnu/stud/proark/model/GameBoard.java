1bee61c41e4fccb74da91dfdbe0482d1baafbb8a
Øyvind Hellenes <hellenes.91@gmail.com>
2014-03-20 16:17:36 +0100

    Laget konstruktor

--- proArkProject/src/no/ntnu/stud/proark/model/GameBoard.java
+++ proArkProject/src/no/ntnu/stud/proark/model/GameBoard.java
@@ -1,11 +1,19 @@
 package no.ntnu.stud.proark.model;
 
+import no.ntnu.stud.proark.model.pieces.GoalPiece;
+import no.ntnu.stud.proark.model.pieces.PlayerPiece;
+
 
 public class GameBoard {
 
-	Difficulty level;
-	
-	public GameBoard(Difficulty level){
+	private Difficulty level;
+	private GoalPiece goalPiece;
+	private PlayerPiece playerPiece;
+
+	public GameBoard(Difficulty level, GoalPiece goalPiece, PlayerPiece playerPiece){
 		this.level = level;
+		this.goalPiece = goalPiece;
+		this.playerPiece = playerPiece;
 	}
+	
 }
