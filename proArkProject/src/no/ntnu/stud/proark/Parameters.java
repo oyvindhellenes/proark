b69d4eaaf1c88401e0c715ac90fad949cc32a7b5
Kyrre <kyrre.l.moe@gmail.com>
2014-03-20 11:29:43 +0100

    made Parameters a Singleton

--- proArkProject/src/no/ntnu/stud/proark/Parameters.java
+++ proArkProject/src/no/ntnu/stud/proark/Parameters.java
@@ -1,8 +1,16 @@
 package no.ntnu.stud.proark;
 
 public class Parameters {
+	public static Parameters instance = null;
 	
 	public Parameters(){
 		
+	}
+	
+	public static Parameters getInstance () {
+		if (instance == null) {
+			instance = new Parameters();
+		}
+		return instance;
 	}
 }
