de67339f5dd9f1ae632ece0819fbb6f81ed73e48
Kyrre <kyrre.l.moe@gmail.com>
2014-03-20 15:05:51 +0100

    added game state

--- proArkProject/src/no/ntnu/stud/proark/states/GameActivity.java
+++ proArkProject/src/no/ntnu/stud/proark/states/GameActivity.java
@@ -5,32 +5,21 @@ import no.ntnu.stud.proark.R.layout;
 import no.ntnu.stud.proark.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
 
-public class MenuActivity extends Activity {
+public class GameActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_menu);
+		setContentView(R.layout.activity_game);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.game, menu);
 		return true;
-	}
-	
-	public void onClickGame(View v){
-		Intent intent = new Intent(this, GameActivity.class);
-		startActivity(intent);
-	}
-	public void onClickSettings(View v){
-		Intent intent = new Intent(this, SettingsActivity.class);
-		startActivity(intent);
 	}
 
 }
