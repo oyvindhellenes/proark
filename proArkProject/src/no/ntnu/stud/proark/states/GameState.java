6c6cf9012456a477c97c573611eeb4980bc1af9b
Kyrre <kyrre.l.moe@gmail.com>
2014-03-26 19:44:07 +0100

    removed genymotion stuff

---
+++ proArkProject/src/no/ntnu/stud/proark/states/GameState.java
@@ -0,0 +1,20 @@
+package no.ntnu.stud.proark.states;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class GameState extends View {
+
+	public GameState(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		super.onDraw(canvas);
+		postInvalidate();
+	}
+}
