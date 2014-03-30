4b0840aa1ab3b39d52ecd10e34438774ad3e6a85
Beate <beatehay@stud.ntnu.no>
2014-03-30 20:51:56 +0200

    dice-enum, updated filenames

---
+++ proArkProject/src/no/ntnu/stud/proark/model/Dice.java
@@ -0,0 +1,22 @@
+package no.ntnu.stud.proark.model;
+
+import no.ntnu.stud.proark.R;
+
+public enum Dice {
+	DICE_ONE(R.drawable.dice_1),
+	DICE_TWO(R.drawable.dice_2),
+	DICE_THREE(R.drawable.dice_3),
+	DICE_FOUR(R.drawable.dice_4),
+	DICE_FIVE(R.drawable.dice_5),
+	DICE_SIX(R.drawable.dice_6);
+	
+	private int imageRef;
+	public int getDiceImage(){
+		return this.imageRef;
+	}
+	Dice(int imageRef){
+		this.imageRef = imageRef;
+	}
+}
+
+
